3c4a2ba451e2b0a1e4ace0c7614f1204f64a879a
Laura <lauracast142@gmail.com>
2025-10-16 15:30:19 -0500

    cambio de nombre de archivo

--- src/components/presentatios/presentation.tsx
+++ src/components/presentatios/presentation.tsx
@@ -7,7 +7,7 @@ import "./presentation.css"
 const Presentation: React.FC = () => {
   const handleDownloadCV = () => {
     // Ruta del PDF en tu carpeta public o assets
-    const pdfPath = "images/curriculum ATS.pdf";
+    const pdfPath = "images/Laura_Castaño_CV.pdf";
     
     // Crear un elemento <a> temporal
     const link = document.createElement("a");
